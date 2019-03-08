f2b8820f0a26f241ed67e411bcbb10cd224ccc82
Alexandre Rivest <rivest123@hotmail.com>
2019-03-08 18:54:31 -0500

    Add basic tab with scrolling scene

--- hackqc/src/App.js
+++ hackqc/src/App.js
@@ -1,26 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import RestoreIcon from '@material-ui/icons/Restore';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import ItemsCarousel from 'react-items-carousel';
 import './App.css';
 
+const styles = {
+  content: {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+  }
+}
+
 class App extends Component {
+
+  state = {
+    tab: 0,
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ tab: value });
+  };
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+        <div style={styles.content}>
+          <ItemsCarousel
+            activeItemIndex={this.state.tab}
+            numberOfCards={1}
           >
-            Learn React
-          </a>
-        </header>
-      </div>
+            <div key={0}>first tab</div>
+            <div key={1}>second tab</div>
+            <div key={2}>thrid tab</div>
+          </ItemsCarousel>
+
+        </div>
+        <BottomNavigation
+          value={this.state.tab}
+          onChange={this.handleChange}
+          showLabels
+        >
+          <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+          <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+          <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+        </BottomNavigation>
+      </div >
     );
   }
 }
