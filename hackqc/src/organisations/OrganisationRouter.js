9eb6ff05f55361e4de6c320619f53d35f064df3b
Alexandre Rivest <rivest123@hotmail.com>
2019-03-08 22:07:57 -0500

    Replace react router for homemade navigation

--- hackqc/src/organisations/OrganisationRouter.js
+++ hackqc/src/organisations/OrganisationRouter.js
@@ -1,18 +1,37 @@
 import React from 'react'
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import ItemsCarousel from 'react-items-carousel';
 import OrgnanisationList from './OrgnanisationList';
-import OrganisationDetail from './OrganisationDetail';
+import OrganisationDetail from './OrganisationDetail'
 
 export default class OrganisationRouter extends React.Component {
 
+  state = {
+    tab: 1,
+    organisation: undefined,
+  }
+
   render() {
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/:id" component={OrganisationDetail} />
-          <Route path="/" component={OrgnanisationList} />
-        </Switch>
-      </BrowserRouter>
+      // <BrowserRouter>
+      //   <Switch>
+      //     <Route exact path="/:id" component={OrganisationDetail} />
+      //     <Route path="/" component={OrgnanisationList} />
+      //   </Switch>
+      // </BrowserRouter>
+      <ItemsCarousel
+        activeItemIndex={this.state.tab}
+        numberOfCards={0}
+      >
+        <OrgnanisationList
+          onOrganisationSelected={(organisation) => this.setState({ tab: 1, organisation: organisation })}
+          key={0} />
+        <OrganisationDetail
+          organisation={this.state.organisation}
+          key={1}
+          onGoBack={() => this.setState({ tab: 0, organisation: undefined })}
+        />
+
+      </ItemsCarousel>
     )
   }
 }
