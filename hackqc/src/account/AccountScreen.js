be6c66c0bbd3947e0750cca6927b28247e745128
Steven Harvey <sharvey@nexapp.ca>
2019-03-09 19:35:02 -0500

    Users

--- hackqc/src/account/AccountScreen.js
+++ hackqc/src/account/AccountScreen.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import {
+  Typography, List, ListItemText, ListItem, Divider, ListItemSecondaryAction, AppBar, Toolbar,
+} from '@material-ui/core';
+import sortBy from 'lodash.sortby';
 import Client from '../Client';
 
 export default class AccountScreen extends React.Component {
@@ -6,7 +10,8 @@ export default class AccountScreen extends React.Component {
     super(props);
 
     this.state = {
-
+      user: {},
+      donations: [],
     };
   }
 
@@ -16,7 +21,10 @@ export default class AccountScreen extends React.Component {
 
   fetchAccount = async () => {
     try {
-      const response = await Client.get('citizens/me');
+      const user = await Client.get('citizens/me');
+      const { donations } = await Client.get('citizens/me/donations');
+      const sortedDonations = sortBy(donations, ['donated_at']).reverse().slice(0, 9);
+      this.setState({ user, donations: sortedDonations });
     } catch (e) {
       console.log(e);
     }
@@ -24,7 +32,37 @@ export default class AccountScreen extends React.Component {
 
   render() {
     return (
-      <div />
+      <div>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" color="inherit">{this.state.user.name}</Typography>
+          </Toolbar>
+        </AppBar>
+
+        <Typography variant="h6" color="inherit" style={{ margin: '3px 0 3px 15px' }}>Historique des dons</Typography>
+
+        <List style={{ marginTop: -7 }}>
+          <Divider />
+
+          {this.state.donations.map(donation => (
+            <div key={donation.reference}>
+              <ListItem
+                button
+                alignItems="flex-start"
+                style={{ backgroundColor: '#FFFFFF' }}
+              >
+                <ListItemText primary={`${donation.amount}$`} />
+                <ListItemSecondaryAction style={{ marginRight: 10 }}>
+                  <span style={{ fontSize: 14, color: '#cccccc' }}>
+                    {(new Date(donation.donated_at)).toLocaleString()}
+                  </span>
+                </ListItemSecondaryAction>
+              </ListItem>
+              <Divider />
+            </div>
+          ))}
+        </List>
+      </div>
     );
   }
 }
